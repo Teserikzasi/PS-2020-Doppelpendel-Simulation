e49d308366833abdeac4d1803a4d5adf422e9d77
Teserikzasi <youtubetesar@gmx.de>
2020-07-15 21:58:43 +0200

    Update Plot-Funktion

--- Auswertung/plot_outputs.m
+++ Auswertung/plot_outputs.m
@@ -1,7 +1,15 @@
-function plot_outputs(out)
-    % Erstellt Plots von x, phi1 und phi2
+function plot_outputs(out, save, name, path, format, resolution)
+    % Erstellt Subplots von x, phi1 und phi2
+    %
+    % Optionale Argumente, um Plot abzuspeichern:
+    %
+    % save = Boolean (Default: false)
+    % name = Dateiname (Default: yyyy-MM-dd_HH-mm-ss_plot)
+    % path = Dateipfad (Default: \Plots)
+    % format = Bildformat (Default: .png)
+    % resolution = Grafikauflösung in DPI (Default: 300)
     
-    % Data konvertieren von 3-dim. zu 1-dim 
+    %% Data konvertieren von 3D zu 1D
     x1 = [];
     phi1 = [];
     phi2 = [];
@@ -11,21 +19,66 @@ function plot_outputs(out)
        phi2(i) = out.mY.Data(2, 1, i);
     end
     
-    % Plotting
-    figure
+    %% Plot
+    fgh = figure();
     
     subplot(3,1,1);
-	plot(out.mY.Time, x1);
-	ylabel('Weg [m]');
+	plot(out.mY.Time, x1, 'Color', [0 0.4470 0.7410]);
+    title('Position x');
+	ylabel('x [m]');
+    axis equal
     
     subplot(3,1,2);
-	plot(out.mY.Time, phi1);
-	ylabel('Winkel 1 [rad]');
+	plot(out.mY.Time, phi1, 'Color', [0.8500 0.3250 0.0980]);
+    title('Winkel \phi_{1}')
+	ylabel('\phi_{1} [rad]');
+    axis equal
     
     subplot(3,1,3);
-	plot(out.mY.Time, phi2);
-	ylabel('Winkel 2 [rad]');
+	plot(out.mY.Time, phi2, 'Color', [0.9290 0.6940 0.1250]);
+    title('Winkel \phi_{2}')
+	ylabel('\phi_{2} [rad]');
+    axis equal
     
     xlabel('Zeit t [s]');
     
+    %% Plot speichern
+    if nargin==1
+        return
+    end
+    % Default-Konfiguration
+    currDate = datetime();
+    currDate.Format = 'yyyy-MM-dd_HH-mm-ss';
+    fileName = [char(currDate) '_' 'plot'];
+    fileSave = false;    
+    filePath = fullfile(cd(), 'Plots');
+    fileFormat = '.png';
+    fileReso = 300;
+    % Argumente prüfen    
+    switch nargin
+        case 2
+            fileSave = save;
+        case 3
+            fileSave = save;
+            fileName = name;
+        case 4
+            fileSave = save;
+            fileName = name;
+            filePath = path;
+        case 5
+            fileSave = save;
+            fileName = name;
+            filePath = path;
+            fileFormat = format;
+        case 6
+            fileSave = save;
+            fileName = name;
+            filePath = path;
+            fileFormat = format;
+            fileReso = resolution;
+    end
+    % speichern
+    if fileSave
+        exportgraphics(fgh, [filePath '\' fileName fileFormat], 'Resolution', fileReso);
+    end
 end
