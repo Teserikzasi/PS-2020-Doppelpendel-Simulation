cba3f22d5aa2b05e32e09a8c3b528149851ffc23
tobi0 <tobi0>
2020-08-18 21:47:51 +0200

    Verlauf Auswertung

--- Auswertung/APAuswertung.m
+++ Auswertung/APAuswertung.m
@@ -4,7 +4,7 @@ function results = APAuswertung(out)
 Tsim = out.tout(end)-out.tout(1);
 F = squeeze(out.vF.Data);
 u = squeeze(out.delta_u.Data);
-x = squeeze(out.delta_x.Data);
+x = squeeze(out.delta_x_real.Data);
 xest = squeeze(out.delta_x_est.Data);
 xesterr = x - xest;
 dt = Tsim/length(u); % Konstante Schrittweite angenommen
@@ -19,12 +19,16 @@ else
     results.stabilised = false;
 end
 
-Q = eye(6);
+Q = diag([10, 1, 10, 1, 10, 1 ]);
 R = 1;
 
-results.Jf = F'*R*F * dt;
+results.Jf = F'*R*F * dt;  % Gütefunktional zur Stellgröße
 results.Ju = u'*R*u * dt;
-results.Jx = sum(diag(x'*Q*x)) * dt;
-results.Jxest = sum(diag(xesterr'*Q*xesterr)) * dt;
+results.Jx = sum(diag(x'*Q*x)) * dt; % Gütefunktional zum Zustandsverlauf
+results.Jxest = sum(diag(xesterr'*Q*xesterr)) * dt; % Gütefunktional zum Zustandsfehler
+
+for i=1:6
+    results.x(i) = AuswertungSignal(x(i,:), 0.05 );
+end
 
 end
