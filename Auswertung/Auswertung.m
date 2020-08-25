379b8014d6af7a4c27fd4a9b08e963edc56d4696
tobi0 <tobi0>
2020-08-25 16:14:04 +0200

    Anpassungen

--- Auswertung/Auswertung.m
+++ Auswertung/Auswertung.m
@@ -1,17 +1,21 @@
-%function Auswertung(out, name, path)
+function Auswertung(out)
+% Allgemeine Auswertung einer Simulation
 
-%path = 'Plots';
-%name = 'motor sättigung u=2';
-plotanimate(out) %name, path)
+if any(out.outofcontrol.Data)
+    t0 = out.outofcontrol.Time(find(out.outofcontrol.Data,1));
+    fprintf('System außer Kontrolle:  t = %.3f\n', t0 )
+end
 
 if any(out.MotorSat_I.Data)
     disp("Motor in Stellgrößenbegrenzung! (Soll Strom > max Strom)")
 end
+
 if any(out.MotorSat_w.Data)
     disp("Motor in Sättigung! (kein max Strom möglich)")
 end
+
 if any(out.satState_u.Data)
     disp("Regler in Stellgrößenbegrenzung! (Motorspannung begrenzt)")
 end
 
-%end
+end
