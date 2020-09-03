acda2dd6e25b7f25fdf279fbd5ce182605fc6370
tobi0 <tobi0>
2020-09-03 23:45:22 +0200

    Beschleunigung Limit (AW für avRegler)

--- Regelung/AP_QR_20_neu.m
+++ Regelung/AP_QR_20_neu.m
@@ -5,6 +5,6 @@ function QRs = AP_QR_20_neu()
 QRs(1) = struct('R', 0.01, 'Q', diag([80, 1, 10, 1, 10, 1 ]) );
 QRs(2) = struct('R', 0.01, 'Q', diag([10, 1, 10, 1, 90, 1 ]) );  % Pendel 2 instabil
 QRs(3) = struct('R', 0.01, 'Q', diag([10, 1, 50, 1, 10, 1 ]) );  % Pendel 1 instabil
-QRs(4) = struct('R', 0.01, 'Q', diag([10, 1, 10, 1, 50, 1 ]) );
+QRs(4) = struct('R', 0.01, 'Q', diag([10, 1, 20, 1, 50, 1 ]) );
 
 end
