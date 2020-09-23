f0510b5e840025ee10b9bbcafeb78b78bc85c6f3
Teserikzasi <youtubetesar@gmx.de>
2020-09-23 17:47:43 +0200

    Erweiterung der Auswertungstools

--- Auswertung/plot_outputs.m
+++ Auswertung/plot_outputs.m
@@ -1,10 +1,7 @@
 function plot_outputs(out, save, name, path, format, resolution)
     % Erstellt Subplots von x, phi1 und phi2
-    % Die Funktion bekommt als Argument "out" die über ToWorkspace
-    % in den Base Workspace abgelegten Ausgänge des Simulink Modells. 
-    % "out" ist eine Struktur, die das Feld "mY" aufweisen muss.
-    % "mY" wiederum enthält die aufgezeichnete Timeseries-Struktur mit 
-    % den Simulationswerten (in ToWorkspace-Block "Timeseries" einstellen)
+    % "out" beinhaltet die Simulationsergebnisse, die mit
+    % ToWorskspace-Blöcken in den Base-Workspace geschrieben wurden
     % 
     % Optionale Argumente, um Plot abzuspeichern:
     %
@@ -28,28 +25,45 @@ function plot_outputs(out, save, name, path, format, resolution)
     phi1 = squeeze(out.mY.Data(2, 1, :));
     phi2 = squeeze(out.mY.Data(3, 1, :));
     Freal = squeeze(out.vF.Data);
+    if out.vF.Time==out.mY.Time
+        vT = out.mY.Time;
+    else
+       disp('out.mY.Time und out.vF.Time haben verschiedene Dimensionen.')
+       return
+    end
+    
     if isfield(out, 'vU')
         Ureal = squeeze(out.vU.Data);
     else
         Ureal = [];
     end
     
-    if ismember('x_est', out.who) % Beobachterschätzwerte (nur bei Regelung)
-        estExist = true;
-        x1_est = squeeze(out.x_est.Data(1, 1, :));
-        phi1_est = squeeze(out.x_est.Data(3, 1, :));
-        phi2_est = squeeze(out.x_est.Data(5, 1, :));
-    else
-        estExist = false;
-    end
-    
-    if ismember('F_soll_reg', out.who) % Daten der Vorsteuerung (bei Regelung)
-        vorst = true;
-        Freg = squeeze(out.F_soll_reg.Data);
-        Fsoll = squeeze(out.F_soll_real.Data);
-        %Usoll = squeeze(out.Usoll.Data);
-    else
-        vorst = false;
+    estExist = false;
+    vorst = false;
+    trajExist = false;
+    try
+        if ismember('x_est', out.who) % Beobachterschätzwerte (nur bei Regelung)
+            estExist = true;
+            x1_est = squeeze(out.x_est.Data(1, 1, :));
+            phi1_est = squeeze(out.x_est.Data(3, 1, :));
+            phi2_est = squeeze(out.x_est.Data(5, 1, :));
+        end
+
+        if ismember('F_soll_reg', out.who) % Daten der Vorsteuerung (bei Regelung)
+            vorst = true;
+            Freg = squeeze(out.F_soll_reg.Data);
+            Fsoll = squeeze(out.F_soll_real.Data);
+            %Usoll = squeeze(out.Usoll.Data);
+        end
+
+        if ismember('mX_traj', out.who)
+            trajExist = true;
+            x1_traj = squeeze(out.mX_traj.Data(1, 1, :));
+            phi1_traj = squeeze(out.mX_traj.Data(3, 1, :));
+            phi2_traj = squeeze(out.mX_traj.Data(5, 1, :));
+        end
+    catch
+        disp('Plots: Abfrage nach x_est, F_soll_reg und mX_traj wurde umgangen.')
     end
     
     %% Plot
@@ -57,51 +71,63 @@ function plot_outputs(out, save, name, path, format, resolution)
     hFig = figure();
     
     subplot(4,1,1);       
-	  plot(out.mY.Time, x1, 'Color', [0 0.4470 0.7410], 'LineWidth', 1);  
+	plot(vT, x1, 'Color', [0 0.4470 0.7410], 'LineWidth', 1);  
     hold on;
     title('Position x');
 	  ylabel('x [m]');
     if estExist
-        plot(out.mY.Time, x1_est, 'Color', [0.3010, 0.7450, 0.9330], 'LineWidth', 1);
+        plot(vT, x1_est, 'Color', [0.3010, 0.7450, 0.9330], 'LineWidth', 1);
         legend('x', 'x_{est}');
+    end
+    if trajExist
+        plot(vT, x1_traj, 'Color', [0.3010, 0.7450, 0.9330], 'LineWidth', 1);
+        legend('x', 'x_{traj}');
     end
     grid on;
     
     subplot(4,1,2);    
-    plot(out.mY.Time, phi1*180/pi, 'Color', [0.8500 0.3250 0.0980], 'LineWidth', 1);
+    plot(vT, rad2deg(phi1), 'Color', [0.8500 0.3250 0.0980], 'LineWidth', 1);
     hold on;	
     title('Winkel \phi_{1}')
 	  ylabel('\phi_{1} [°]');
     if estExist
-        plot(out.mY.Time, phi1_est*180/pi, 'Color', [0.9290, 0.6940, 0.1250], 'LineWidth', 1);
+        plot(vT, rad2deg(phi1_est), 'Color', [0.9290, 0.6940, 0.1250], 'LineWidth', 1);
         legend('\phi_{1}', '\phi_{1-est}'); 
-    end   
+    end 
+    if trajExist
+        plot(vT, rad2deg(phi1_traj), 'Color', [0.9290, 0.6940, 0.1250], 'LineWidth', 1);
+        legend('\phi_{1}', '\phi_{1-traj}'); 
+    end
     grid on;
     
     subplot(4,1,3);
-    plot(out.mY.Time, phi2*180/pi, 'Color', [0.4940, 0.1840, 0.5560], 'LineWidth', 1);   
+    plot(vT, rad2deg(phi2), 'Color', [0.4940, 0.1840, 0.5560], 'LineWidth', 1);   
     hold on;	
     title('Winkel \phi_{2}')
 	  ylabel('\phi_{2} [°]');
     if estExist
-        plot(out.mY.Time, phi2_est*180/pi, 'Color', [0.4660, 0.6740, 0.1880], 'LineWidth', 1);
+        plot(vT, rad2deg(phi2_est), 'Color', [0.4660, 0.6740, 0.1880], 'LineWidth', 1);
         legend('\phi_{2}', '\phi_{2-est}'); 
     end    
+    if trajExist
+        plot(vT, rad2deg(phi2_traj), 'Color', [0.4660, 0.6740, 0.1880], 'LineWidth', 1);
+        legend('\phi_{2}', '\phi_{2-traj}'); 
+    end   
     grid on;
     
     subplot(4,1,4);
     hold on; 
     if vorst
-        plot(out.tout, Freg, 'Color', [1, 0.5, 0.1], 'LineWidth', 1);
-        plot(out.tout, Fsoll, 'Color', [0.8, 0.078, 0.184], 'LineWidth', 1);
-        plot(out.tout, Freal, 'Color', [0.3, 0.5, 0.9], 'LineWidth', 1); 
+        plot(vT, Freg, 'Color', [1, 0.5, 0.1], 'LineWidth', 1);
+        plot(vT, Fsoll, 'Color', [0.8, 0.078, 0.184], 'LineWidth', 1);
+        plot(vT, Freal, 'Color', [0.3, 0.5, 0.9], 'LineWidth', 1); 
         legend('F_{reg}', 'F_{soll}', 'F_{out}');
     elseif ~isempty(Ureal)        
-        plot(out.tout, Ureal*staticGain, 'Color', [0.8, 0.078, 0.184], 'LineWidth', 1);
-        plot(out.tout, Freal, 'Color', [0.3, 0.5, 0.9], 'LineWidth', 1); 
+        plot(vT, Ureal*staticGain, 'Color', [0.8, 0.078, 0.184], 'LineWidth', 1);
+        plot(vT, Freal, 'Color', [0.3, 0.5, 0.9], 'LineWidth', 1); 
         legend('U_{in}*MotGain', 'F_{out}')
     else
-        plot(out.tout, Freal, 'Color', [0.3, 0.5, 0.9], 'LineWidth', 1); 
+        plot(vT, Freal, 'Color', [0.3, 0.5, 0.9], 'LineWidth', 1); 
     end
     title('Stellgröße (Kraft F)');
 	ylabel('F [N]');
