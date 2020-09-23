d90cd26455b42c207b0cf13ce344fa5bbd6c2c7e
tobi0 <tobi0>
2020-09-23 19:14:09 +0200

    save_system

--- Regelung/InitVorstBeob_Fa.m
+++ Regelung/InitVorstBeob_Fa.m
@@ -5,12 +5,15 @@ function InitVorstBeob_Fa(sysF)
 % Mithilfe von matlabFunctionBlock wird die symbolische Gleichung in eine 
 % matlab Funktion geschrieben. 
 % Dazu muss Simulink geöffnet sein und es muss danach gespeichert werden.
+% Mit save_system wird automatisch gespeichert.
 
 equation_a = sysF.f(2); % Gleichung für die Beschleunigung x0_pp
 equation_F = solve(str2sym('a')==equation_a, str2sym('F')); % Gleichung für die Kraft in Abh. von a
 matlabFunctionBlock('SchlittenGleichungBeschleunigung/x0_pp', equation_a );
 matlabFunctionBlock('SchlittenGleichungKraft/F0', equation_F );
+save_system('SchlittenGleichungBeschleunigung');
+save_system('SchlittenGleichungKraft');
 
-disp('Simulink Module für Vorsteuerung/Beobachter F<->a aktualisiert (evtl Speichern notwendig)')
+disp('Simulink Module für Vorsteuerung/Beobachter F<->a aktualisiert')% (evtl Speichern notwendig)
 
 end
