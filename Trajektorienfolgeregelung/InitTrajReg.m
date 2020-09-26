1277a959689bda512798ce363934c3b2570f36bb
Teserikzasi <youtubetesar@gmx.de>
2020-09-26 17:04:03 +0200

    Ausbau der Trajektorienfolgeregelung

--- Trajektorienfolgeregelung/InitTrajReg.m
+++ Trajektorienfolgeregelung/InitTrajReg.m
@@ -1,12 +1,19 @@
 % Initialisierung der Trajektorienfolgeregelung
 global TrajRegData
 global sysF;
+global simparams
 searchPath = 'Trajektorien\searchResults\Results_odeTesGeb_app09_maxIt10000\Euler_MPC';
-fileName = 'Traj23_dev0_3.14_3.14_x0max0.8';
+fileName = 'Traj14_dev0_-3.14_-3.14_x0max0.8.mat';
 trj = load(fullfile(searchPath, fileName));
 vT = 0 : trj.T : (trj.T)*(trj.N);
-stTraj.T.data = vT;
+stTraj.T.data = vT';
 stTraj.X.data = (trj.results_traj.x_traj)';
-stTraj.U.data = trj.results_traj.u_traj;
+stTraj.U.data = [trj.results_traj.u_traj; 0];
+stTraj.Y.data = stTraj.X.data(:,[1 3 5]);
 [regData, beoData] = Traj_QR();
 TrajRegData = calcTrajRegData(sysF, stTraj, regData, beoData);
+TrajRegData.T.data = stTraj.T.data;
+
+
+
+simparams.TrajRegDataF = TrajRegData;
