9dd02a236435ba505233b20a0211e5cfe51b13a4
tobi0 <tobi0>
2020-09-30 20:12:09 +0200

    neuer QR Vergleich

--- Regelung/QR_Test_vergleich.m
+++ Regelung/QR_Test_vergleich.m
@@ -1,13 +1,13 @@
 
 simparams.Zustandsermittlung = 1;  % ZM
 
+tic
 InitAPRegData(AP_QR_Chang19())
 resC = x0_Test_APs();
-
 InitAPRegData(AP_QR_Ribeiro20())
 resR = x0_Test_APs();
-
 InitAPRegData(AP_QR_20_neu())
 resN = x0_Test_APs();
+toc
 
 plot_x0_APs([resC resR resN], ["Chang19","Ribeiro20","20neu"] )
